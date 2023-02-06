fe6bbc2a746a9b4878f3790ea7d5845547b7c8c0
kruser <kruser@yandex.ru>
2023-02-06 19:33:05 +0500

    added generate for any period

--- schedule.py
+++ schedule.py
@@ -1,6 +1,3 @@
-import os
-import time
-
 import requests
 from PIL import Image, ImageDraw, ImageFont
 from environs import Env
@@ -14,6 +11,7 @@ MONTH = ['ЯНВАРЯ', 'ФЕВРАЛЯ', 'МАРТА', 'АПРЕЛЯ',
          'ОКТЯБРЯ', 'НОЯБРЯ', 'ДЕКАБРЯ']
 WEEKDAYS = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']
 UTC = 5
+
 
 def get_token(api_url, api_key):
     url = api_url + '/auth/token'
@@ -30,8 +28,8 @@ def get_schedule(api_url, token, date_start, date_end):
     url = api_url + '/schedule'
 
     params = {
-        'dateStart': date_start, # YYYY-MM-DD
-        'dateEnd': date_end, # YYYY-MM-DD
+        'dateStart': date_start,  # YYYY-MM-DD
+        'dateEnd': date_end,  # YYYY-MM-DD
     }
 
     headers = {
@@ -43,16 +41,15 @@ def get_schedule(api_url, token, date_start, date_end):
     return response.json()
 
 
-def fetch_next_week_dates(date) -> list[str]: # format dates YYYY-MM-DD
-
-    delta_for_thursday =  4 - date.isoweekday()
+def fetch_next_week_dates(date) -> list[str]:  # format dates YYYY-MM-DD
+    delta_for_thursday = 4 - date.isoweekday()
 
     start_date = date + timedelta(days=delta_for_thursday)
-    week_dates = []
+    week = []
     for day in range(7):
-        week_dates.append(str(start_date + timedelta(days=day)))
+        week.append(str(start_date + timedelta(days=day)))
 
-    return week_dates
+    return week
 
 
 def dd_month(str_date):
@@ -60,8 +57,24 @@ def dd_month(str_date):
     return f'{day} {MONTH[int(month) - 1]}'
 
 
-def formate_schedule(schedule, films):
+def fetch_one_two_pcice(period):
+    filters = [(date.fromisoformat(day).isoweekday() in (5, 6, 7)) for day in period]
 
+    if all(filters):
+        low = filters.index(True)
+        high = -1
+    elif not any(filters):
+        low = filters.index(False)
+        high = -1
+    else:
+        low = filters.index(False)
+        high = filters.index(True)
+
+    return low, high
+
+
+
+def formate_schedule(schedule, films):
     formatted_schedule = {}
     for day, schedule_day in schedule.items():
         formatted_schedule[day] = {}
@@ -92,24 +105,31 @@ def draw_text_schedule(template, period=None, schedule=None):
     start_date = dd_month(period[0])
     end_date = dd_month(period[-1])
 
+    low, high = fetch_one_two_pcice(period)
+    print(low, high)
+    one_price = True
+    if high >= 0:
+        one_price = False
+
     img = Image.open(template)
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("tahomabd.ttf", 48)
     font_small = ImageFont.truetype("tahomabd.ttf", 38)
-    font_long_25= ImageFont.truetype("tahomabd.ttf", 36)
-    font_long_35= ImageFont.truetype("tahomabd.ttf", 30)
+    font_long_25 = ImageFont.truetype("tahomabd.ttf", 36)
+    font_long_35 = ImageFont.truetype("tahomabd.ttf", 30)
     font_text = ImageFont.truetype("tahoma.ttf", 36)
 
     draw.text((1280, 180), start_date, (0, 0, 0), font=font)
     draw.text((1280, 280), end_date, (0, 0, 0), font=font)
 
-    draw.text((1400, 350), "ЧТ,ПН-СР", (0, 0, 0), font=font_text)
-    draw.text((1580, 350), "ПТ-ВС", (0, 0, 0), font=font_text)
+    if not one_price:
+        draw.text((1400, 350), "ЧТ,ПН-СР", (0, 0, 0), font=font_text)
+        draw.text((1580, 350), "ПТ-ВС", (0, 0, 0), font=font_text)
 
-    start_halls = dict(zip(('1','2','3'), (410, 1060, 1720)))
+    start_halls = dict(zip(('1', '2', '3'), (410, 1060, 1720)))
 
     # fill one day schedule and price
-    for hall, seances in schedule[week_dates[0]].items():
+    for hall, seances in schedule[period[low]].items():
         start = start_halls[hall]
         for index, seance in enumerate(seances):
             pos_y = start + index * 80
@@ -125,21 +145,24 @@ def draw_text_schedule(template, period=None, schedule=None):
                 draw.text((380, pos_y + 5), seance['name'], (0, 0, 0), font=font_long_25)
             draw.text((1130, pos_y + 5), seance['rate'], (0, 0, 0), font=font_small)
             draw.text((1250, pos_y + 5), seance['chrono'], (0, 0, 0), font=font_small)
-            draw.text((1440, pos_y), seance['price'], (0, 0, 0), font=font)
+            if one_price:
+                draw.text((1520, pos_y), seance['price'], (0, 0, 0), font=font)
+            else:
+                draw.text((1440, pos_y), seance['price'], (0, 0, 0), font=font)
 
     # fill weekend price
-    for hall, seances in schedule[week_dates[2]].items():
-        start = start_halls[hall]
-        for index, seance in enumerate(seances):
-            pos_y = start + index * 80
-            draw.text((1600, pos_y), seance['price'], (0, 0, 0), font=font)
+    if not one_price:
+        for hall, seances in schedule[period[high]].items():
+            start = start_halls[hall]
+            for index, seance in enumerate(seances):
+                pos_y = start + index * 80
+                draw.text((1600, pos_y), seance['price'], (0, 0, 0), font=font)
 
     img.save('test.jpg')
     img.show()
 
 
 if __name__ == '__main__':
-
     env = Env()
     env.read_env()
     api_key = env.str('API_KEY')
@@ -159,13 +182,12 @@ if __name__ == '__main__':
         film['kinoplan_id']: [film['name'], film['rate']] for film in films
     }
 
-    schedule_by_date_by_hall = { day: {
+    schedule_by_date_by_hall = {day: {
             hall['title']: list(filter(lambda x: x['date'] == day and x['hall_id'] == hall['id'], schedule))
-                for hall in halls
+            for hall in halls
         } for day in week_dates}
 
     formatted_schedule = formate_schedule(schedule_by_date_by_hall, serialized_films)
 
-    draw_text_schedule(template, week_dates, formatted_schedule)
-
-
+    # draw_text_schedule(template, week_dates, formatted_schedule)  # полная неделя
+    # draw_text_schedule(template, week_dates[:5], formatted_schedule)  # период 0 - четверг, 7 - среда
