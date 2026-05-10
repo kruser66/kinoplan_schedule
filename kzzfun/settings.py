9b8a8953e7ec30606333133c5bfe9eeb5fe7d4a2
Serg Kruglov <kruser@yandex.ru>
2026-05-10 13:00:33 +0500

    fixed hosts

--- kzzfun/settings.py
+++ kzzfun/settings.py
@@ -14,8 +14,9 @@ SECRET_KEY = env.str('SECRET_KEY', 'Secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', '*.pythonanywhere.com'])
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', '*.kruser.su'])
+# Настройки для поддомена
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', ['https://kzzfun.kruser.su'])
 
 # Application definition
 
